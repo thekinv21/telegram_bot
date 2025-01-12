3f24b6c62bc28c42f7ce09f3614c702dcfaa4175
Vadim Kiniabaev <thekinv21@gmail.com>
2025-01-12 15:18:41 +0300

    feat:[master-xxx][Vadim]:  Set up global API prefix, exception handling, and Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import * as dotenv from 'dotenv'
+
 import { NestFactory } from '@nestjs/core'
+import type { NestExpressApplication } from '@nestjs/platform-express'
+import { SwaggerModule } from '@nestjs/swagger'
+
 import { AppModule } from './app.module'
+import { ApiResponseInterceptor, GlobalExceptionFilter } from './common'
+import { swagger } from './config'
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
-	await app.listen(process.env.PORT ?? 4200)
+	const app = await NestFactory.create<NestExpressApplication>(AppModule)
+
+	app.setGlobalPrefix('api')
+	app.useGlobalFilters(new GlobalExceptionFilter())
+	app.useGlobalInterceptors(new ApiResponseInterceptor())
+
+	SwaggerModule.setup('docs', app, SwaggerModule.createDocument(app, swagger))
+
+	await app.listen(process.env.APP_PORT || 4200)
+
+	dotenv.config()
 }
 bootstrap()
